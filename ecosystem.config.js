0015d7f3dde59695883b37ca6fec786ec62d61d7
Dilsara Thiranjaya <dilsarathiranjaya3@gmail.com>
2026-03-19 15:52:55 +0530

    chore: commit all the current changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,28 +1,20 @@
 module.exports = {
   apps : [
     {
-      name: 'book-service',
-      script: 'java',
-      args: '-jar book-service/target/book-service-1.0.0.jar',
-      instances: 1,
-      autorestart: true,
-      max_memory_restart: '1G'
+      name: "book-service",
+      script: "java -jar ./book-service/target/book-service-1.0.0.jar",
+      log_file: "./logs/book-service.log",
     },
     {
-      name: 'member-service',
-      script: 'java',
-      args: '-jar member-service/target/member-service-1.0.0.jar',
-      instances: 1,
-      autorestart: true,
-      max_memory_restart: '1G'
+      name: "member-service",
+      script: "java -jar ./member-service/target/member-service-1.0.0.jar",
+      log_file: "./logs/member-service.log",
+
     },
     {
-      name: 'file-service',
-      script: 'java',
-      args: '-jar file-service/target/file-service-1.0.0.jar',
-      instances: 1,
-      autorestart: true,
-      max_memory_restart: '1G'
+      name: "file-service",
+      script: "java -jar ./file-service/target/file-service-1.0.0.jar",
+      log_file: "./logs/file-service.log",
     }
   ]
 };
