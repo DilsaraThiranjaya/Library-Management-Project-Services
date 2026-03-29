7bd968a70b8138534324a5cd61e9a3550769cbd7
Dilsara Thiranjaya <dilsarathiranjaya3@gmail.com>
2026-03-29 09:09:08 +0530

    chore: update ecosystem config for services

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,17 +4,20 @@ module.exports = {
       name: "book-service",
       script: "java -jar ./book-service/target/book-service-1.0.0.jar",
       log_file: "./logs/book-service.log",
+      instances: 2,
     },
     {
       name: "member-service",
       script: "java -jar ./member-service/target/member-service-1.0.0.jar",
       log_file: "./logs/member-service.log",
+      instances: 2,
 
     },
     {
       name: "file-service",
       script: "java -jar ./file-service/target/file-service-1.0.0.jar",
       log_file: "./logs/file-service.log",
+      instances: 2,
     }
   ]
 };
